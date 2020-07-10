fb155b38cbea3f035c494ddbd8944eda8b9ec5a4
Faisal Animashaun <d.f.animashaun@gmail.com>
2020-07-09 20:54:54 -0400

    Delete script.py

--- portfolio/src/main/java/com/google/sps/servlets/script.py
+++
@@ -1,6 +0,0 @@
-import requests
-url = ('http://newsapi.org/v2/top-headlines?'
-       'country=us&'
-       'apiKey=96317a20b027446da228dce495ce6af7')
-response = requests.get(url)
-print(response.json())
